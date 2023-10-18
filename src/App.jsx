e9bc1304328cd7a82b9711a114beab11bff6040a
PASALASURESH <124240418+pasalasu@users.noreply.github.com>
2023-10-18 09:20:47 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Sidebar from './layout/Sidebar/Sidebar';
+import Context from './layout/Content/Content'
+function App() {
+ 
+
+  return (
+    <>
+      <div className='app'>
+     <Sidebar/>
+     <Context/>
+      </div>
+    </>
+  )
+}
+
+export default App
